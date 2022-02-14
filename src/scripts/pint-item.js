8dc84bf7449060fba155c334d8f7458891175290
Enminsk <enminsk@yandex.ru>
2022-02-14 22:45:39 +0300

    fix(modal): fix run modal

--- src/scripts/pint-item.js
+++ src/scripts/pint-item.js
@@ -1,26 +1,4 @@
 // Logic For one Pint Item
-const btnOpenModal = document.querySelector('#show-modal');
-const closeModal = document.querySelector('.modal');
-const modal = new Modal(document.querySelector('.modal'))
-
-btnOpenModal.addEventListener('click', () => {
-    modal.open();
-});
-closeModal.addEventListener('click', () => {
-    modal.close();
-});
-
-function Modal(root) {
-    this.root = root;
-
-    this.open = function () {
-        this.root.style.display = 'flex';
-    }
-    this.close = function () {
-        this.root.style.display = 'none'
-    }
-}
-
 
 function RenderOnePint({}) {
     this.root = null;
