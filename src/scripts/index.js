483c91e0bd356cf06010603ec2e9d237913f39b2
Enminsk <enminsk@yandex.ru>
2022-03-08 23:23:18 +0300

    init(DropdownMenu): add DropdownMenu in header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,14 @@
 import { modalСomplaint, modaladd } from "./components/Modals.js";
 import { allPints } from "./components/Pint-item.js";
 import { toast } from "./components/Toast.js";
+import { dropdownMenu } from "./components/Dropdown.js"
 
 app();
 
 function app () {
   allPints;
   toast;
+  dropdownMenu;
   modalСomplaint.init();
   modaladd.init();
 }
