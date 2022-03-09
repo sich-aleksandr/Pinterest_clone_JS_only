42a7397a7f7854194494d1c6c912dfea2b1fb9ad
sich-aleksandr <sich.aleksandr@gmail.com>
2022-03-09 12:08:27 +0300

    refactor(AD-4) rename toast and pint file

--- src/scripts/components/toast.js
+++ src/scripts/components/toast.js
@@ -1,6 +1,6 @@
 function Toast(options) {
   this.init = function () {
-    this.root = document.querySelector('#toast');
+    this.root = document.querySelector("#toast");
     this.root.addEventListener("click", this.handleToast);
     this.start();
   };
@@ -32,3 +32,7 @@ function Toast(options) {
 const toast = new Toast().init();
 
 export { toast };
+
+
+
+
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,17 @@
 import { modalСomplaint, modaladd } from "./components/Modals.js";
 import { allPints } from "./components/Pint-item.js";
 import { toast } from "./components/Toast.js";
-import { dropdownMenu } from "./components/Dropdown.js"
+import { dropdownMenu } from "./components/Dropdown.js";
 
 app();
 
-function app () {
+
+
+
+function app() {
   allPints;
   toast;
   dropdownMenu;
   modalСomplaint.init();
   modaladd.init();
 }
-
-
