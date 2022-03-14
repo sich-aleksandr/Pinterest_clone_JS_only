8cbbc6ddd3113cd48a359734a0c33dca7c812f88
sich-aleksandr <sich.aleksandr@gmail.com>
2022-03-14 18:46:24 +0300

    refactor(PC-16) turn off butto load more for desks

--- src/scripts/components/Dropdown.js
+++ src/scripts/components/Dropdown.js
@@ -4,9 +4,7 @@ const selectDesk = document
   .querySelector(".dropdown-menu-content")
   .addEventListener("change", function () {
     displayOneDesk(this.value);
-    document.querySelector("#load-more").classList.add("btn--close");
-    console.log(document.querySelector(".card__menu-button"));
-    
+    document.querySelector("#load-more").classList.add("btn--close"); 
   });
 
   export { selectDesk }
