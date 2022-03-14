d9f6ccc336a1663c14e13ac1ef80eb353716813b
sich-aleksandr <sich.aleksandr@gmail.com>
2022-03-14 11:59:53 +0300

    init(PC-16) add event for drop down menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,20 @@
 import { menu } from "./components/Menu";
 import { modalAdd } from "./components/ModalAddDesk.js";
 import { modalСomplaint } from "./components/ModalComplaints.js";
-import { renderPints, displayOneDesk} from "./components/Pint-item.js";
-import { getPints, getPint } from "./services/mockAPI.js";
+import { renderPints, displayOneDesk } from "./components/Pint-item.js";
+import { getPints} from "./services/mockAPI.js";
 import { toast } from "./components/Toast.js";
-
 
 document.addEventListener("DOMContentLoaded", app);
 
-document.querySelector('#test-pint').addEventListener("click", () => {
-  document.querySelector('.pin_container').innerHTML = "";
-  displayOneDesk(0);
- 
-})
-
-
-function displayOneDesk (n) {
-  const pinst = localDataList.get();
-  pinst[n].forEach(element => { 
-    getPint(element).then(data => {
-      const getData = [data];
-      renderPints(getData)
-    }); 
+document
+  .querySelector(".dropdown-menu-content")
+  .addEventListener("change", function () {
+    document.querySelector(".pin_container").innerHTML = "";
+    displayOneDesk(this.value);
   });
-}
 
-function app () {
+function app() {
   getPints().then(renderPints);
   toast; // load toast on start of app
 
