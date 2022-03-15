5eafaa2805c634a88759670d0556f3f2ab87d787
sich-aleksandr <sich.aleksandr@gmail.com>
2022-03-15 12:53:44 +0300

    fix(PC-17) bug with dropdown menu

--- src/scripts/components/Dropdown.js
+++ src/scripts/components/Dropdown.js
@@ -3,8 +3,13 @@ import { displayOneDesk } from "./Pint-item.js";
 const selectDesk = document
   .querySelector(".dropdown-menu-content")
   .addEventListener("change", function () {
-    displayOneDesk(this.value);
-    document.querySelector("#load-more").classList.add("btn--close"); 
+    if (this.value === '333') {
+      document.location.href ="/";
+      return;
+    } else {
+      displayOneDesk(this.value);
+      document.querySelector("#load-more").classList.add("btn--close");
+    }
   });
 
-  export { selectDesk }
+export { selectDesk };
