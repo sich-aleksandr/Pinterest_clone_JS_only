6cd88508a8e3a067e138dc90881efa2b0366b31a
sich-aleksandr <sich.aleksandr@gmail.com>
2022-03-15 12:59:40 +0300

    feat(PC-17) event lisener for mobile select

--- src/scripts/components/Dropdown.js
+++ src/scripts/components/Dropdown.js
@@ -1,7 +1,7 @@
 import { displayOneDesk } from "./Pint-item.js";
 
 const selectDesk = document
-  .querySelector(".dropdown-menu-content")
+  .querySelector("#desktop-select")
   .addEventListener("change", function () {
     if (this.value === '333') {
       document.location.href ="/";
@@ -12,4 +12,16 @@ const selectDesk = document
     }
   });
 
-export { selectDesk };
+const selectMobile = document
+  .querySelector("#mobile-select")
+  .addEventListener("change", function () {
+    if (this.value === '333') {
+      document.location.href ="/";
+      return;
+    } else {
+      displayOneDesk(this.value);
+      document.querySelector("#load-more").classList.add("btn--close");
+    }
+  });
+
+export { selectDesk, selectMobile };
